6c2cca6ce0bd326ae2d2e5f2e0af87da1acb9ab4
Sho Kusano <rosylilly@aduca.org>
2020-04-04 08:58:26 +0900

    Add tests

--- test/dummy/app/models/application_record.rb
+++ test/dummy/app/models/application_record.rb
@@ -2,4 +2,13 @@
 
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+
+  def self.i18n_scope
+    @i18n_scope || super
+  end
+
+  def self.i18n_scope=(scope)
+    @i18n_scope = scope
+  end
 end
--- test/lib/generators/locale_kit/locale_kit/model_generator_test.rb
+++ test/lib/generators/locale_kit/locale_kit/model_generator_test.rb
@@ -10,6 +10,26 @@ module LocaleKit
     test 'generator runs without errors' do
       run_generator ['User']
       assert_file 'app/locales/models/user/ja.yml'
+
+      path = destination_root.join('app/locales/models/user/ja.yml')
+      content = File.read(path)
+
+      assert_includes(content, "ja:\n  activerecord:")
+    end
+
+    test 'generator runs with custom scope' do
+      ApplicationRecord.i18n_scope = :minitest
+
+      run_generator ['User']
+      assert_file 'app/locales/models/user/ja.yml'
+
+      ApplicationRecord.i18n_scope = nil
+
+      path = destination_root.join('app/locales/models/user/ja.yml')
+      content = File.read(path)
+
+      assert_includes(content, "ja:\n  minitest:")
+
     end
   end
 end
